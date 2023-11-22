23cdec968b38e6c559db2a9203ac0fb07c5d429f
NikolaGrudov <138569112+NikolaGrudov@users.noreply.github.com>
2023-11-22 10:58:37 +0200

    Add files via upload

---
+++ Boost_tests/foundGradesTest.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_DYN_LINK 
+#define BOOST_TEST_MODULE MoveSemantics
+#include <boost/test/included/unit_test.hpp>
+#include "vector"
+BOOST_AUTO_TEST_SUITE(foundGrades)
+BOOST_AUTO_TEST_CASE(foundGrades)
+{
+    std::vector<double> gr {5,2,2, 3.3};
+    Student s1("Ivan", 18, 5.5, gr);
+
+    
+    std::vector<double> fgrades{};
+    BOOST_CHECK(s1.foundGrades(gr, fgrades) == true);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
