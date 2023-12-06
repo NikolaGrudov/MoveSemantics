1b20fc155e8a72d0b502111b40fde7420b88593b
NikolaGrudov <138569112+NikolaGrudov@users.noreply.github.com>
2023-12-06 14:37:59 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -2,22 +2,9 @@
 #define __Student_h__
 #include <vector>
 #include <iostream>
+#include "Subject.h"
 
-class Subject
-{
-public:
-    Subject(const char* nameOfSubject, const char* nameOfTeacher);
-    size_t getSubLen();
-    size_t getTeaLen();
-    const char* getSubName() const;
-    const char* getTeaName() const;
 
-private:
-    char* m_nameOfSubject;
-    char* m_nameOfTeacher;
-    size_t subject_len;
-    size_t teacher_len;
-};
 
 class Student
 {
