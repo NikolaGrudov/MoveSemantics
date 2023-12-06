c983812aa1fd5b0e7c657abb289f65d6281a7332
NikolaGrudov <138569112+NikolaGrudov@users.noreply.github.com>
2023-12-06 14:37:09 +0200

    Update foundGradesTest.cpp

--- Boost_tests/foundGradesTest.cpp
+++ Boost_tests/foundGradesTest.cpp
@@ -3,13 +3,18 @@
 #include <boost/test/unit_test.hpp>
 #include "vector"
 #include <Student.h>
+#include <Subject.h>
 
 BOOST_AUTO_TEST_SUITE(foundGrades)
 BOOST_AUTO_TEST_CASE(foundGrades)
 {
     std::vector<double> gr {5,2,2, 3.3};
     std::vector <Subject*> subjects1;
+    
+    Subject *maths = new Subject("Maths", "Gogev");
+    subjects1.push_back(maths);
     Student s1("Ivan", 18, 5.5, gr, subjects1);
+
     
     std::vector<double> fgrades{};
     BOOST_CHECK(s1.foundGrades(gr, fgrades) == true);
@@ -22,7 +27,11 @@ BOOST_AUTO_TEST_SUITE(averageGrades)
 BOOST_AUTO_TEST_CASE(averageGrades)
 {
     const std::vector<double> gr {5,5,4,4};
-    Student s1("Ivan", 18, 5.5, gr);
+    std::vector <Subject*> subjects1;
+    
+    Subject *maths = new Subject("Maths", "Gogev");
+    subjects1.push_back(maths);
+    Student s1("Ivan", 18, 5.5, gr, subjects1);
     BOOST_CHECK(s1.averageGrades(gr) == 4.5);
 }
 BOOST_AUTO_TEST_SUITE_END()
