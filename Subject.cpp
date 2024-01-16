9aa2c5fb5f4a5b1d5eab4921c8972225394d7919
NikolaGrudov <138569112+NikolaGrudov@users.noreply.github.com>
2024-01-16 13:55:53 +0200

    Update Subject.cpp

--- Subject.cpp
+++ Subject.cpp
@@ -6,27 +6,31 @@
 
 
 Subject::Subject(const char* nameOfSubject, const char* nameOfTeacher): subject_len(strlen(nameOfSubject)), teacher_len(strlen(nameOfTeacher))
-    {
-        m_nameOfSubject = new char[subject_len+1];
-        strcpy(m_nameOfSubject, nameOfSubject);
-        m_nameOfTeacher = new char[teacher_len+1];
-        strcpy(m_nameOfTeacher, nameOfTeacher);
-    }
+{
+    m_nameOfSubject = new char[subject_len+1];
+    strcpy(m_nameOfSubject, nameOfSubject);
+    m_nameOfTeacher = new char[teacher_len+1];
+    strcpy(m_nameOfTeacher, nameOfTeacher);
+}
 
-    size_t Subject::getSubLen()
-    {
-        return subject_len;
-    }
-    size_t Subject::getTeaLen()
-    {
-        return teacher_len;
-    }
-    const char* Subject::getSubName() const
-    {
-        return m_nameOfSubject;
-    }
-    const char* Subject::getTeaName() const
-    {
-        return m_nameOfTeacher;
-    }
-    
+size_t Subject::getSubLen()
+{
+    return subject_len;
+}
+size_t Subject::getTeaLen()
+{
+    return teacher_len;
+}
+const char* Subject::getSubName() const
+{
+    return m_nameOfSubject;
+}
+const char* Subject::getTeaName() const
+{
+    return m_nameOfTeacher;
+}
+
+void Subject::printSubject(const Subject& s) const
+{
+
+}
